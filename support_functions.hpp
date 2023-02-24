a5c149e0630101bce3e785a1fdaa2f3bc56ea5d2
Amata Dhātu <70560694+LeKhang97@users.noreply.github.com>
2023-02-24 11:55:13 +0100

    Add files via upload
    Added distance information option

--- support_functions.hpp
+++ support_functions.hpp
@@ -24,6 +24,7 @@ vector<string> json_value_to_vector(auto json_value){
     }
     return result;
 }
+
 void print_1d_array(vector<string> input_array){
     for(string ele: input_array){
         cout << ele << '\t';
@@ -55,16 +56,17 @@ vector<vector<string>> duplicate_vector(vector<string> input_vector, int number,
 }
 
 Shortest_path BFS(map<string, vector<string>> graph, string startNode, string endNode, 
-bool distance_only = false){
+bool distance_only = false, json dist_dict = nullptr){
     vector<string> visited = {startNode}, queue = {startNode}, keys, lastnodes = {startNode};
-    int s = 0, flag = 1;
+    int s = 0, flag = 1, dist_exist = 0;
     vector<vector<string>> visited_list;
     visited_list.push_back({startNode});
     Shortest_path shortestpath;
     shortestpath.distance = -1;
     shortestpath.start = startNode;
     shortestpath.end = endNode;
-    
+    vector<string> discarded_neighbors;
+
     //Check if startNode = endNode and interact with itself
     if(startNode == endNode){
         if(find(graph[startNode].begin(), graph[startNode].end(), startNode) == graph[startNode].end()){
@@ -78,12 +80,31 @@ bool distance_only = false){
         return shortestpath;
     }
 
+    /*Demo dist_dict
+    dist_dict["0"] = {{"0", 0}, {"1", 1}, {"4", 2}, {"7", 2}, {"11", 3}};
+    dist_dict["3"] = {{"4", 1}, {"5", 3}, {"7", 2}};
+    dist_dict["11"] = {{"7", 2}};
+
+    cout << dist_dict << endl;
+    */
+
+    vector<string> wordListVector = {startNode, endNode};
+    
+    sort( wordListVector.begin(), wordListVector.end());
+    
+    if(dist_dict.contains(wordListVector.front())){
+        if(dist_dict[wordListVector[0]].contains(wordListVector[1])){
+            dist_exist = 1;
+        }
+    }
+    
     keys.reserve(graph.size()); // to preallocate enough memory for the new vector
     for(const auto [key, value] : graph) {
         keys.push_back(key);
     }
 
     while(flag) {
+        cout << "\nChecking at distance:\t" << s+1 << endl;
         vector<string> sublastnodes;
         vector<string> next_queue;
 
@@ -92,49 +113,130 @@ bool distance_only = false){
             string currentNode = queue.front();
             
             queue.erase(queue.begin());
-            
+            //cout << endl;
             if(find(keys.begin(), keys.end(), currentNode) != keys.end()){
+                //cout << "currentNode:\t" << currentNode << endl;
                 for(string neighbor: graph[currentNode]){
+                    int add_neighbor = 0;
                     if( find(visited.begin(), visited.end(), neighbor) == visited.end() &&
-                        find(lastnodes.begin(), lastnodes.end(), neighbor) == lastnodes.end()){
-                        temp_visited_list.push_back(neighbor);
-                        if(find(next_queue.begin(), next_queue.end(), neighbor) == next_queue.end()){
-                            next_queue.push_back(neighbor);
+                        find(lastnodes.begin(), lastnodes.end(), neighbor) == lastnodes.end() &&
+                        find(discarded_neighbors.begin(), discarded_neighbors.end(), neighbor) == discarded_neighbors.end()){
+                        /// Start check sum of distance 
+                        if(dist_exist){
+                            vector<string> list_3_nodes = {startNode, endNode, neighbor};
+                            //cout << "checking neighbor:\t" << neighbor;
+                            sort(list_3_nodes.begin(), list_3_nodes.end());              
+
+                            if((list_3_nodes.front() == startNode && list_3_nodes.back() == endNode)||
+                            (list_3_nodes.back() == startNode && list_3_nodes.front() == endNode)){
+                                if(dist_dict[list_3_nodes[0]].contains(neighbor) && dist_dict[neighbor].contains(list_3_nodes[2])){
+                                    if(int(dist_dict[list_3_nodes[0]][list_3_nodes[1]]) + int(dist_dict[list_3_nodes[1]][list_3_nodes[2]]) == 
+                                    int(dist_dict[list_3_nodes[0]][list_3_nodes[2]])){
+                                        //cout << int(dist_dict[list_3_nodes[0]][list_3_nodes[1]]) << endl;
+                                        //cout << int(dist_dict[list_3_nodes[1]][list_3_nodes[2]]) << endl;
+                                        //cout << int(dist_dict[list_3_nodes[0]][list_3_nodes[2]]) << endl;
+                                        add_neighbor = 1;
+                                    }
+                                    // Check why cannot add this neighbor
+                                    else{
+                                        discarded_neighbors.push_back(neighbor);
+                                    }
+                                    // End check
+                                }else{
+                                    add_neighbor = 1;
+                                }
+                            }
+                            else{
+                                if(list_3_nodes.front() == neighbor){
+                                    if(dist_dict.contains(neighbor) && dist_dict[neighbor].contains(list_3_nodes[2])){
+                                        if(int(dist_dict[list_3_nodes[0]][list_3_nodes[1]]) + int(dist_dict[list_3_nodes[0]][list_3_nodes[2]]) == 
+                                        int(dist_dict[list_3_nodes[1]][list_3_nodes[2]])){
+                                            add_neighbor = 1;
+                                        }
+                                        // Check why cannot add this neighbor
+                                        else{
+                                            discarded_neighbors.push_back(neighbor);
+                                        }
+                                        // End check
+                                    }else{
+                                        add_neighbor = 1;
+                                    }
+                                }
+                                else{
+                                    if(dist_dict[list_3_nodes[0]].contains(neighbor) && dist_dict[list_3_nodes[1]].contains(neighbor)){
+                                        if(int(dist_dict[list_3_nodes[0]][list_3_nodes[2]]) + int(dist_dict[list_3_nodes[1]][list_3_nodes[2]]) == 
+                                        int(dist_dict[list_3_nodes[0]][list_3_nodes[1]])){
+                                            add_neighbor = 1;
+                                        }
+                                        // Check why cannot add this neighbor
+                                        else{
+                                            discarded_neighbors.push_back(neighbor);
+                                        }
+                                        // End check
+                                    }else{
+                                        add_neighbor = 1;
+                                    }
+                                }
+                            }
+                        } else{
+                            //cout << "distance between start and end node does not exist" << endl;
+                            //cout << "checking neighbor:\t" << neighbor;
+                            add_neighbor = 1;
                         }
-                        if(neighbor == endNode){
-                            shortestpath.distance = s + 1;
-                            flag = 0;
-                            if(distance_only == true){
-                                return shortestpath;
+                        /// End check sum of distance
+                        if(add_neighbor){
+                            //cout << "\t(added to queue)" << endl;
+                            temp_visited_list.push_back(neighbor);
+                        
+                            if(find(next_queue.begin(), next_queue.end(), neighbor) == next_queue.end()){
+                                next_queue.push_back(neighbor);
+                            }
+                            if(neighbor == endNode){
+                                shortestpath.distance = s + 1;
+                                flag = 0;
+                                if(distance_only == true){
+                                    return shortestpath;
+                                }
                             }
                         }
+                        /*else{
+                             cout << "\t(discarded)" << endl;
+                        }*/
                     }
                 }
 
                 if(distance_only == false){
                     vector<vector<string>> v = visited_list;
                     int u = 0;
+                    int t = 0; 
                     if(v.size() > 0){
                         for(int index = 0; index < v.size(); index++){
                             int a = visited_list.size() - v.size();
                             if((v[index].back() == currentNode)){
-                                vector<vector<string>> m = duplicate_vector(v[index], temp_visited_list.size(),temp_visited_list);
-                                if(m.size()){
-                                    for(int i = v.size() - 1; i > index + u -a -1 ; i--){
-                                        visited_list.pop_back();
-                                    }
-                                    for(vector<string> sub_m :m){
-                                        visited_list.push_back(sub_m);
-                                        if(sub_m.back() == endNode){
-                                            shortestpath.path.push_back(sub_m);
+                                // Remove paths which cannot be expanded (death end)
+                                if(temp_visited_list.size() == 0){
+                                    visited_list.erase(visited_list.begin() + index - t);
+                                    t ++;
+                                } 
+                                else{
+                                    vector<vector<string>> m = duplicate_vector(v[index], temp_visited_list.size(),temp_visited_list);
+                                    if(m.size()){
+                                        for(int i = v.size() - 1; i > index + u -a -1 ; i--){
+                                            visited_list.pop_back();
                                         }
-                                    }
-                                    if(v.size() - (index+u+1) > 0){
-                                        for(int i = index+u+1-a ; i < v.size(); i++){
-                                            visited_list.push_back(v[i]);
+                                        for(vector<string> sub_m :m){
+                                            visited_list.push_back(sub_m);
+                                            if(sub_m.back() == endNode){
+                                                shortestpath.path.push_back(sub_m);
+                                            }
                                         }
+                                        if(v.size() - (index+u+1) > 0){
+                                            for(int i = index+u+1-a ; i < v.size(); i++){
+                                                visited_list.push_back(v[i]);
+                                            }
+                                        }
+                                        u += m.size() - 1;
                                     }
-                                    u += m.size() - 1;
                                 }
                             }
                         }
@@ -163,6 +265,8 @@ bool distance_only = false){
         next_queue.clear();
         s ++;
     }
+    //cout << "visited paths are:" << endl;
+    //print_2d_array(visited_list);
     return shortestpath;
 }
 
@@ -182,6 +286,7 @@ class argument{
         string start = "NULL";
         string end = "NULL";
         string graph = "NULL";
+        string distance_information = "NULL";
         string outfile = "NULL";
         int flag_d = 0;
 };
@@ -198,10 +303,11 @@ argument parsing_argv(int argc, char** argv){
             {"end", required_argument, NULL, 'e'},
             {"graph", optional_argument, NULL, 'g'},
             {"distance_only", no_argument, NULL, 'd'},
+            {"distance_information", no_argument, NULL, 'i'},
             {"outfile", optional_argument, NULL, 'o'},
             {NULL, 0, NULL, 0}
         };
-        int c = getopt_long(argc, argv, "hs:e:g:do:", long_options, &option_index);
+        int c = getopt_long(argc, argv, "hs:e:g:di:o:", long_options, &option_index);
         if(c == -1){
             break;
         }
@@ -224,8 +330,9 @@ argument parsing_argv(int argc, char** argv){
             cout << "-g [ --graph ]\t\t\tAdjacency dictionary in json format. If graph is not provided, the program will use a demo graph.\n";
             cout << "-o [ --outfile ]\t\tExport result as json format file.\n";
             cout << "-d [ --distance_only ]\t\tOnly return shortest distance, not the path.\n";
+            cout << "-i [ --distance_information ]\t\tPerform the BFS faster with the known distances stored in json format file.\n";
             
-            exit(EXIT_FAILURE);
+            exit(EXIT_SUCCESS);
         case 's':
             vm.start = optarg;
             break;
@@ -237,6 +344,9 @@ argument parsing_argv(int argc, char** argv){
             break;
         case 'd':
             vm.flag_d = 1;
+            break;
+        case 'i':
+            vm.distance_information = optarg;
             break;
         case 'o':
             vm.outfile = optarg;
